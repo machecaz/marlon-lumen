a3a6148588851686b9d8d1eca3d02bb14af96d12
danieletulone <danieletulone.work@gmail.com>
2021-03-12 00:29:30 +0100

    Fix provider

--- src/Providers/MarlonLumenServiceProvider.php
+++ src/Providers/MarlonLumenServiceProvider.php
@@ -8,29 +8,35 @@ use Validator;
 
 class MarlonLumenServiceProvider extends ServiceProvider
 {
-    $this->app->resolving(FormRequest::class, function ($req, $app) {
-        $validator = Validator::make(
-            request()->all(),
-            $req->rules(),
-        );
+    public function register() {
 
-        if ($validator->fails()) {
-            throw new ValidationException($validator);
-        }
+    }
 
-        if (count($validator->validated()) < count(request()->all())) {
-            foreach (request()->all() as $key => $value) {
-                if (!isset($validator->validated()[$key])) {
-                    $validator->errors()->add(
-                        $key,
-                        'This field is not accepted from current request. Please remove it.'
-                    );
-                }
+    public function boot() {
+        $this->app->resolving(FormRequest::class, function ($req, $app) {
+            $validator = Validator::make(
+                request()->all(),
+                $req->rules(),
+            );
+    
+            if ($validator->fails()) {
+                throw new ValidationException($validator);
             }
-
-            throw new ValidationException($validator);
-        }
-
-        $req->replace($validator->validated());
-    });
+    
+            if (count($validator->validated()) < count(request()->all())) {
+                foreach (request()->all() as $key => $value) {
+                    if (!isset($validator->validated()[$key])) {
+                        $validator->errors()->add(
+                            $key,
+                            'This field is not accepted from current request. Please remove it.'
+                        );
+                    }
+                }
+    
+                throw new ValidationException($validator);
+            }
+    
+            $req->replace($validator->validated());
+        });
+    }
 }
