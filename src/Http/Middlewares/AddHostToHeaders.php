7421c6c97c9555886e9aeb7b8ce3b48c43988df1
danieletulone <danieletulone.work@gmail.com>
2021-03-12 21:58:16 +0100

    Add middleware for adding host to headers

---
+++ src/Http/Middlewares/AddHostToHeaders.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Marlon\Lumen\Http\Middlewares;
+
+use Closure;
+
+class AddHostToHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        $response = $next($request);
+
+        $response->header('Host', config('app.url'));
+
+        return $response;
+    }
+}
